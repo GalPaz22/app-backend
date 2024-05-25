df6e2d044886d27bf80287aed284f5a2f3da628d
GalPaz <galpaz2210@gmail.com>
2024-05-25 14:30:39 +0300

    js-cookies8

--- server.mjs
+++ server.mjs
@@ -14,7 +14,7 @@ import MongoStore from 'connect-mongo';
 const app = express();
 const port = 4000;
 process.env.ANTHROPIC_API_KEY =
-  "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";
+  "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";
 
 const mongoUri = 'mongodb+srv://galpaz2210:jGqI4pEv3gZuJTCc@cluster0.qiplrsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
