c9e94fd41bc37f0381d088556abe87e1fad0335c
GalPaz <galpaz2210@gmail.com>
2024-05-28 23:31:19 +0300

    session- try37

--- server.mjs
+++ server.mjs
@@ -148,7 +148,7 @@ app.post("/logout", async (req, res) => {
 
     // Update user's active session to an empty string
     await usersCollection.updateOne(
-      { _id: new MongoClient(userId) },
+      { _id: new MongoClient.ObjectId(userId) },
       { $set: { activeSession: "" } }
     );
 
