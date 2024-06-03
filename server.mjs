9a781570cf473a2e9f09655ba441613f9834f20f
GalPaz <galpaz2210@gmail.com>
2024-06-03 17:54:56 +0300

    embed2

--- server.mjs
+++ server.mjs
@@ -176,6 +176,12 @@ app.post("/logout", async (req, res) => {
   res.status(500).send("Internal Server Error");
   }
   });
+  function cosineSimilarity(a, b) {
+    const dotProduct = a.reduce((sum, val, i) => sum + val * b[i], 0);
+    const normA = Math.sqrt(a.reduce((sum, val) => sum + val * val, 0));
+    const normB = Math.sqrt(b.reduce((sum, val) => sum + val * val, 0));
+    return dotProduct / (normA * normB);
+  }
 
 
   app.post("/generate-response", upload.single("file"), async (req, res) => {
