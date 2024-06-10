071fa025306299789d770bceb1b027364f0e8aca
GalPaz <galpaz2210@gmail.com>
2024-06-10 16:32:35 +0300

    new2

--- server.mjs
+++ server.mjs
@@ -292,7 +292,7 @@ app.post('/chat-response', async (req, res) => {
         res.write(`data: ${JSON.stringify(token.choices[0].delta.content)}\n\n`);
         console.log(token.choices[0].delta.content);
         }
-      res.end();
+    
     }
     
 
