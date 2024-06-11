2738120f50a8d9aa06fedc9697595736f681ac0b
GalPaz <galpaz2210@gmail.com>
2024-06-11 19:38:17 +0300

    new2

--- server.mjs
+++ server.mjs
@@ -46,7 +46,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "https://ask-your-doc.vercel.app", 
+    origin: "*", 
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE"],
