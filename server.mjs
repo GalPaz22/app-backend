2ccae2ee280194d984200c8abdae4084dcc48114
GalPaz <galpaz2210@gmail.com>
2024-06-12 14:58:51 +0300

    new2

--- server.mjs
+++ server.mjs
@@ -184,7 +184,7 @@ app.post("/generate-response", upload.single("file"), async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
+    
 
     const conversationHistory = sessionMemory[currentSessionId] || [];
     conversationHistory.push(`User: ${question}`);
