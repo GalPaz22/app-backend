1ca64fe1c952c3e95891bdcd4a821df4927897d7
GalPaz <galpaz2210@gmail.com>
2024-06-13 16:51:34 +0300

    new2

--- server.mjs
+++ server.mjs
@@ -197,6 +197,7 @@ app.post("/generate-response", upload.single("file"), async (req, res) => {
     const embeddings = new VoyageEmbeddings({
       apiKey: process.env.VOYAGEAI_API_KEY, // In Node.js defaults to process.env.VOYAGEAI_API_KEY
       inputType: "document", // Optional: specify input type as 'query', 'document', or omit for None / Undefined / Null
+      model: "voyage-multilingual-2"
     });
     
     const pinecone = new Pinecone({
