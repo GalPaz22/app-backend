e06532b4049f2e5cc5f3b590e00a45462eb1ff53
GalPaz <galpaz2210@gmail.com>
2024-06-16 11:29:54 +0300

    new2

--- server.mjs
+++ server.mjs
@@ -182,8 +182,7 @@ app.post("/embed-pdf", upload.single("file"), async (req, res) => {
   // Generate a unique session ID for this document
 
   try {
-    if (pineNamespace) {
-      await pineNamespace.deleteAll();}
+   
     const loader = new PDFLoader(filePath, { splitPages: false });
     const docs = await loader.load();
     if (!docs || docs.length === 0 || !docs[0].pageContent) {
